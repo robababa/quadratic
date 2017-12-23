6a223892ef0a6b09425139e79362d313fe0543ac
Rob Johnson <robj@hightouchinc.com>
2017-12-23 13:10:45 -0600

    Export almost everything so I can use it
    I might be able to pare this back later.

--- quadratic.go
+++ quadratic.go
@@ -5,95 +5,95 @@ import (
 	"sort"
 )
 
-type solution interface {
-	always() bool
-	never() bool
-	sometimes() bool
-	values() []int
+type Solution interface {
+	Always() bool
+	Never() bool
+	Sometimes() bool
+	Values() []int
 }
 
-type never struct {}
+type Never struct {}
 
-func (_ never) always() bool {
+func (_ Never) Always() bool {
 	return false
 }
 
-func (_ never) never() bool {
+func (_ Never) Never() bool {
 	return true
 }
 
-func (_ never) sometimes() bool {
+func (_ Never) Sometimes() bool {
 	return false
 }
 
-func (_ never) values() []int {
+func (_ Never) Values() []int {
 	var answer []int
 	return answer
 }
 
-type always struct {}
+type Always struct {}
 
-func (_ always) always() bool {
+func (_ Always) Always() bool {
 	return true
 }
 
-func (_ always) never() bool {
+func (_ Always) Never() bool {
 	return false
 }
 
-func (_ always) sometimes() bool {
+func (_ Always) Sometimes() bool {
 	return false
 }
 
-// return an empty array. The caller needs to check always() first
-func (_ always) values() []int {
+// return an empty array. The caller needs to check Always() first
+func (_ Always) Values() []int {
 	var answer []int
 	return answer
 }
 
-type sometimes struct {
+type Sometimes struct {
 	solutionValues []int
 }
 
-func (_ sometimes) always() bool {
+func (_ Sometimes) Always() bool {
 	return false
 }
 
-func (_ sometimes) never() bool {
+func (_ Sometimes) Never() bool {
 	return false
 }
 
-func (_ sometimes) sometimes() bool {
+func (_ Sometimes) Sometimes() bool {
 	return true
 }
 
-func (s sometimes) values() []int {
+func (s Sometimes) Values() []int {
 	return s.solutionValues
 }
 
-func combineSolutions(solutions ...solution) solution {
-	// if we have no solutions, return never
-	if len(solutions) == 0 {return never{}}
+func CombineSolutions(solutions ...Solution) Solution {
+	// if we have no solutions, return Never
+	if len(solutions) == 0 {return Never{}}
 
-	// check for never among the solutions
+	// check for Never among the solutions
 	for _, sol := range solutions {
-		if sol.never() {return never{}}
+		if sol.Never() {return Never{}}
 	}
-	// check for always
+	// check for Always
 	alwaysForAll := true
 	for _, sol := range solutions {
-		if !sol.always() {
+		if !sol.Always() {
 			alwaysForAll = false
 			break
 		}
 	}
-	if alwaysForAll {return always{}}
+	if alwaysForAll {return Always{}}
 
 	// we have work to do. Identify the candidates, sort them, and see if one works
 	var candidates []int
 	for _, sol := range solutions {
-		if sol.sometimes() {
-			candidates = append(candidates, sol.values()...)
+		if sol.Sometimes() {
+			candidates = append(candidates, sol.Values()...)
 			}
 	}
 	sort.Ints(candidates)
@@ -102,39 +102,39 @@ CandidateLoop:
 	for _, c := range candidates {
 		SolutionsLoop:
 		for _, sol := range solutions {
-			// if this solution is valid for all positive integers, then great, go to the next solution!
-			if sol.always() {continue SolutionsLoop}
-			for _, val := range sol.values() {
-				// if the candidate is a value in the solution, then great, go to the next solution!
+			// if this Solution is valid for all positive integers, then great, go to the next Solution!
+			if sol.Always() {continue SolutionsLoop}
+			for _, val := range sol.Values() {
+				// if the candidate is a value in the Solution, then great, go to the next Solution!
 				if val == c {continue SolutionsLoop}
 			}
-			// didn't find the candidate in this solution, go to the next candidate
+			// didn't find the candidate in this Solution, go to the next candidate
 			continue CandidateLoop
 		}
 		// this candidate worked for all solutions, so it is the lowest positive integer that works.  Return it!
-		return sometimes{solutionValues: []int{c}}
+		return Sometimes{solutionValues: []int{c}}
 	}
 	// no candidates worked
-	return never{}
+	return Never{}
 }
 
-// returns the positive integer solution to the equation bx + c = 0, if one exists
-// if it is always true for all positive x, then it returns AlwaysZero
+// returns the positive integer Solution to the equation bx + c = 0, if one exists
+// if it is Always true for all positive x, then it returns AlwaysZero
 // if it is true for no positive integers x, then it returns NoSolutionValue
-func linearPositiveIntegerSolution(b int, c int) solution {
+func LinearPositiveIntegerSolution(b int, c int) Solution {
 	switch {
-	// 0 = 0 is always true
-	case b == 0 && c == 0: return always{}
-		// c == 0 is always false when c != 0
-	case b == 0 && c != 0: return never{}
+	// 0 = 0 is Always true
+	case b == 0 && c == 0: return Always{}
+		// c == 0 is Always false when c != 0
+	case b == 0 && c != 0: return Never{}
 		// bx = 0 for b != 0 is true iff x is 0
-	case b != 0 && c == 0: return sometimes{solutionValues: []int{0}}
+	case b != 0 && c == 0: return Sometimes{solutionValues: []int{0}}
 	default: {
 		candidate := int(-1 * c / b)
 		if candidate > 0 && b * candidate + c == 0 {
-			return sometimes{solutionValues: []int{candidate}}
+			return Sometimes{solutionValues: []int{candidate}}
 		} else {
-			return never{}
+			return Never{}
 		}
 	}
 	}
@@ -143,25 +143,25 @@ func linearPositiveIntegerSolution(b int, c int) solution {
 // returns the positive integer solutions to the quadratic equation
 // ax^2 + bx + c = 0
 // if there are two solutions, this function returns both of them
-// if there is only one positive integer solution, it returns that solution and NoSolutionValue
+// if there is only one positive integer Solution, it returns that Solution and NoSolutionValue
 // if there are no positive integer solutions, the function returns NoSolutionValue, NoSolutionValue
-func quadraticPositiveIntegerSolutions(a int, b int, c int) solution {
+func QuadraticPositiveIntegerSolutions(a int, b int, c int) Solution {
 	// first, get some edge cases out of the way when a = 0
 	if a == 0 {
-		return linearPositiveIntegerSolution(b, c)
+		return LinearPositiveIntegerSolution(b, c)
 	}
 
 	// at this point, we have a real quadratic equation
 	discriminant := b * b - 4 * a * c
 	// imaginary solutions don't work here
 	if discriminant < 0 {
-		return never{}
+		return Never{}
 	}
 
 	// the discriminant is nonnegative.  However, if its root is not an integer, we have no solutions
 	discriminantRoot := int(math.Sqrt(float64(discriminant)))
 	if discriminantRoot * discriminantRoot != discriminant {
-		return never{}
+		return Never{}
 	}
 
 	// put our potential solutions in ascending order
@@ -181,10 +181,10 @@ func quadraticPositiveIntegerSolutions(a int, b int, c int) solution {
 	}
 
 	if len(reallyWorks) == 0 {
-		return never{}
+		return Never{}
 	}
 
-	// reallyWorks has one or two elements, so there are some solution values
-	return sometimes{reallyWorks}
+	// reallyWorks has one or two elements, so there are some Solution Values
+	return Sometimes{reallyWorks}
 }
 
--- quadratic_test.go
+++ quadratic_test.go
@@ -3,12 +3,12 @@ package quadratic
 import "fmt"
 
 func linearHelper(b, c int) {
-	sol := linearPositiveIntegerSolution(b, c)
-	fmt.Println(b, ",", c, "always(), never(), values() are", sol.always(), sol.never(), sol.values())
+	sol := LinearPositiveIntegerSolution(b, c)
+	fmt.Println(b, ",", c, "Always(), Never(), Values() are", sol.Always(), sol.Never(), sol.Values())
 }
 
 func ExampleLinearPositiveIntegerSolution() {
-	// 2x - 6 = 0 : x = 3 is a solution
+	// 2x - 6 = 0 : x = 3 is a Solution
 	linearHelper(2, -6)
 
 	// 2x - 5 = 0 : x = 2.5 is not an integer, so there are no solutions
@@ -17,23 +17,23 @@ func ExampleLinearPositiveIntegerSolution() {
 	// 2x + 6 = 0 : x = -3 is not positive, so there are no solutions
 	linearHelper(2, 6)
 
-	// 2x = 0 : x = 0 is a solution
+	// 2x = 0 : x = 0 is a Solution
 	linearHelper(2, 0)
 
-	// 0x = 0 : always is the solution
+	// 0x = 0 : Always is the Solution
 	linearHelper(0, 0)
 
 	// Output:
-	// 2 , -6 always(), never(), values() are false false [3]
-	// 2 , -5 always(), never(), values() are false true []
-	// 2 , 6 always(), never(), values() are false true []
-	// 2 , 0 always(), never(), values() are false false [0]
-	// 0 , 0 always(), never(), values() are true false []
+	// 2 , -6 Always(), Never(), Values() are false false [3]
+	// 2 , -5 Always(), Never(), Values() are false true []
+	// 2 , 6 Always(), Never(), Values() are false true []
+	// 2 , 0 Always(), Never(), Values() are false false [0]
+	// 0 , 0 Always(), Never(), Values() are true false []
 }
 
 func quadraticHelper(a, b, c int) {
-	sol := quadraticPositiveIntegerSolutions(a, b, c)
-	fmt.Println(a, ",", b, ",", c, "always(), never(), values() are", sol.always(), sol.never(), sol.values())
+	sol := QuadraticPositiveIntegerSolutions(a, b, c)
+	fmt.Println(a, ",", b, ",", c, "Always(), Never(), Values() are", sol.Always(), sol.Never(), sol.Values())
 }
 
 func ExampleQuadraticPositiveIntegerSolutions() {
@@ -41,7 +41,7 @@ func ExampleQuadraticPositiveIntegerSolutions() {
 	quadraticHelper(1, -4, 3)
 	// x^2 - 4 = 0 : x = 2
 	quadraticHelper(1, 0, -4)
-	// x^2 - 4x + 4 = 0 : x = 2 is a double solution, but should only appear once
+	// x^2 - 4x + 4 = 0 : x = 2 is a double Solution, but should only appear once
 	quadraticHelper(1, -4, 4)
 	// x^2 + 4x + 3 = 0 : no solutions
 	quadraticHelper(1, 4, 3)
@@ -58,39 +58,39 @@ func ExampleQuadraticPositiveIntegerSolutions() {
 
 	fmt.Println()
 	fmt.Println("a = b = c = 0")
-	// 0 = 0 : always
+	// 0 = 0 : Always
 	quadraticHelper(0, 0, 0)
 	// Output:
-	// 1 , -4 , 3 always(), never(), values() are false false [1 3]
-	// 1 , 0 , -4 always(), never(), values() are false false [2]
-	// 1 , -4 , 4 always(), never(), values() are false false [2]
-	// 1 , 4 , 3 always(), never(), values() are false true []
+	// 1 , -4 , 3 Always(), Never(), Values() are false false [1 3]
+	// 1 , 0 , -4 Always(), Never(), Values() are false false [2]
+	// 1 , -4 , 4 Always(), Never(), Values() are false false [2]
+	// 1 , 4 , 3 Always(), Never(), Values() are false true []
 	//
 	// a = 0:
-	// 0 , 1 , -7 always(), never(), values() are false false [7]
+	// 0 , 1 , -7 Always(), Never(), Values() are false false [7]
 	//
 	// a = b = 0, c nonzero
-	// 0 , 0 , 2 always(), never(), values() are false true []
+	// 0 , 0 , 2 Always(), Never(), Values() are false true []
 	//
 	// a = b = c = 0
-	// 0 , 0 , 0 always(), never(), values() are true false []
+	// 0 , 0 , 0 Always(), Never(), Values() are true false []
 }
 
 func ExampleCombineSolutions() {
-	fmt.Println("combineSolutions(always{}, always{}).always() =",
-		combineSolutions(always{}, always{}).always())
-	fmt.Println("combineSolutions(always{}, never{}).never() =",
-		combineSolutions(always{}, never{}).never())
-	fmt.Println("combineSolutions(always{}, sometimes{solutionValues: []int{5, 4}}).values() =",
-		combineSolutions(always{}, sometimes{solutionValues: []int{5, 4}}).values())
-	fmt.Println("combineSolutions(never{}, sometimes{solutionValues: []int{5, 4}}).never() =",
-		combineSolutions(never{}, sometimes{solutionValues: []int{5, 4}}).never())
-	fmt.Println("combineSolutions(sometimes{solutionValues: []int{3, 9, 7}}, sometimes{solutionValues: []int{5, 7, 4}}).values() =",
-		combineSolutions(sometimes{solutionValues: []int{3, 9, 7}}, sometimes{solutionValues: []int{5, 7, 4}}).values())
+	fmt.Println("CombineSolutions(Always{}, Always{}).Always() =",
+		CombineSolutions(Always{}, Always{}).Always())
+	fmt.Println("CombineSolutions(Always{}, Never{}).Never() =",
+		CombineSolutions(Always{}, Never{}).Never())
+	fmt.Println("CombineSolutions(Always{}, Sometimes{solutionValues: []int{5, 4}}).Values() =",
+		CombineSolutions(Always{}, Sometimes{solutionValues: []int{5, 4}}).Values())
+	fmt.Println("CombineSolutions(Never{}, Sometimes{solutionValues: []int{5, 4}}).Never() =",
+		CombineSolutions(Never{}, Sometimes{solutionValues: []int{5, 4}}).Never())
+	fmt.Println("CombineSolutions(Sometimes{solutionValues: []int{3, 9, 7}}, Sometimes{solutionValues: []int{5, 7, 4}}).Values() =",
+		CombineSolutions(Sometimes{solutionValues: []int{3, 9, 7}}, Sometimes{solutionValues: []int{5, 7, 4}}).Values())
 	// Output:
-	// combineSolutions(always{}, always{}).always() = true
-	// combineSolutions(always{}, never{}).never() = true
-	// combineSolutions(always{}, sometimes{solutionValues: []int{5, 4}}).values() = [4]
-	// combineSolutions(never{}, sometimes{solutionValues: []int{5, 4}}).never() = true
-	// combineSolutions(sometimes{solutionValues: []int{3, 9, 7}}, sometimes{solutionValues: []int{5, 7, 4}}).values() = [7]
+	// CombineSolutions(Always{}, Always{}).Always() = true
+	// CombineSolutions(Always{}, Never{}).Never() = true
+	// CombineSolutions(Always{}, Sometimes{solutionValues: []int{5, 4}}).Values() = [4]
+	// CombineSolutions(Never{}, Sometimes{solutionValues: []int{5, 4}}).Never() = true
+	// CombineSolutions(Sometimes{solutionValues: []int{3, 9, 7}}, Sometimes{solutionValues: []int{5, 7, 4}}).Values() = [7]
 }
